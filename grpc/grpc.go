1158924b1240d8dd3dac9ed27072220ece0884ac
苏宁 <q178149@163.com>
2024-02-24 10:02:31 +0800

    return service

--- grpc/grpc.go
+++ grpc/grpc.go
@@ -8,18 +8,20 @@ import (
 	"net"
 )
 
-func RegisterGRPC(port int) {
+func RegisterGRPC(port int) (*grpc.Server, error) {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-		panic(err)
+		return nil, err
 	}
 	s := grpc.NewServer()
 	//反射接口支持查询
 	reflection.Register(s)
+
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
-		panic(err)
+		return nil, err
 	}
+	return nil, err
 }
