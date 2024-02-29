e2512727d77e42bc9527f12a22a287b23d0cef16
苏宁 <q178149@163.com>
2024-02-29 11:53:49 +0800

    tls

--- grpc/grpc.go
+++ grpc/grpc.go
@@ -3,19 +3,26 @@ package grpc
 import (
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
 )
 
-func RegisterGRPC(port int, register func(s *grpc.Server)) error {
+func RegisterGRPC(port int, register func(s *grpc.Server), cert, key string) error {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 		return err
 	}
 
-	s := grpc.NewServer()
+	// Create tls based credential.
+	creds, err := credentials.NewServerTLSFromFile(cert, key)
+	if err != nil {
+		log.Fatalf("failed to create credentials: %v", err)
+	}
+
+	s := grpc.NewServer(grpc.Creds(creds))
 	//反射接口支持查询
 	reflection.Register(s)
 	register(s)
